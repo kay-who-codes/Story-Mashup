04e4ae98771c7e1ce115ff73a955ccbc543a5b3d
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2025-03-28 08:19:34 +0000

    Create prompt1.js

---
+++ prompts/prompt1.js
@@ -0,0 +1,6 @@
+const prompts1 = [
+  "A mysterious stranger arrives in town...",
+  "The clock struck midnight, and everything changed...",
+  "In a world where magic is outlawed...",
+  "The letter arrived 50 years late..."
+];
